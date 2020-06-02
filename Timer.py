87cbc156c2ba2df082c44d138d38d85b5933ac19
AdrianSaati <64697142+AdrianSaati@users.noreply.github.com>
2020-06-02 15:40:16 +0200

    Update Timer.py

--- Timer.py
+++ Timer.py
@@ -2,24 +2,16 @@ import time
 import subprocess
 from os import system
 
-
 def countdown(n):
     n=int(n)
     while n>0:
           print(n)
           time.sleep(1)
           n=n-1
-         # os.system('clear')
-          
-         
-          
-
+          os.system('clear')
     else:
         print('Finish')
         subprocess.call(['afplay','bell.wav'])
-
-
-
+        
 n=input('time in seconds: ')
 countdown(n)
-
