bb1fb0b193a23822709bb4955ca00f0af88fd142
AdrianSaati <64697142+AdrianSaati@users.noreply.github.com>
2020-06-02 15:31:58 +0200

    Add files via upload

---
+++ Timer.py
@@ -0,0 +1,25 @@
+import time
+import subprocess
+from os import system
+
+
+def countdown(n):
+    n=int(n)
+    while n>0:
+          print(n)
+          time.sleep(1)
+          n=n-1
+         # os.system('clear')
+          
+         
+          
+
+    else:
+        print('Finish')
+        subprocess.call(['afplay','bell.wav'])
+
+
+
+n=input('time in seconds: ')
+countdown(n)
+
